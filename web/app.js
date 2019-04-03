62b6c9084c6ed782caa043665e78ce2286cfb735
Micha Surber <msurber@futurelab.ch>
2019-04-03 14:07:26 +0200

    Load the pdf url from the hash to avoid reloading
    Somehow the whole pdf.js was completely reloaded for each pdf that the
    user viewed. Using the hash of the url should help avoiding this.
    The url should be viewer.html#file=/path/to/file.pdf

--- web/app.js
+++ web/app.js
@@ -2183,7 +2183,7 @@ function webViewerInitialized() {
   const appConfig = PDFViewerApplication.appConfig;
   let file;
   if (typeof PDFJSDev === "undefined" || PDFJSDev.test("GENERIC")) {
-    const queryString = document.location.search.substring(1);
+    const queryString = document.location.hash.substring(1);
     const params = parseQueryString(queryString);
     file = params.get("file") ?? AppOptions.get("defaultUrl");
     validateFileURL(file);
