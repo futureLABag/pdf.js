1344194b75f08be32e0cc0483e4a4f7cff21f684
Micha Surber <msurber@futurelab.ch>
2019-04-04 11:31:41 +0200

    Hide the print button, introduce fl_allow_export
    Some of our customers don’t want to allow their users to print a preview
    of a report. They need to create a document to print it. So in the
    report editor we need to hide the print option. We still want to be able
    to print from the normal PDF detail view so we introduce
    fl_export_allowed this hash parameter can be set to ‚true‘ and then the
    print buttons will be shown.

--- web/app.js
+++ web/app.js
@@ -2186,6 +2186,10 @@ function webViewerInitialized() {
     const queryString = document.location.hash.substring(1);
     const params = parseQueryString(queryString);
     file = params.get("file") ?? AppOptions.get("defaultUrl");
+
+    if (params.export === 'true') {
+      document.body.classList.add('fl_export_allowed');
+    }
     validateFileURL(file);
   } else if (PDFJSDev.test("MOZCENTRAL")) {
     file = window.location.href;
