8e57596f3a27a6a52a7e40509254f8dfb603e46c
Micha Surber <msurber@futurelab.ch>
2019-04-04 11:33:04 +0200

    Show active search when configured in hash
    The default pdf.js viewer does look for a search hash in the url. If it
    is set the pdf is searched for the value of the param. To show the user
    that the search is active we also wan’t to show them the search field
    with their values inside.

--- web/pdf_link_service.js
+++ web/pdf_link_service.js
@@ -315,6 +315,8 @@ class PDFLinkService {
           source: this,
           query: phrase ? query : query.match(/\S+/g),
         });
+        PDFViewerApplication.findBar.open();
+        PDFViewerApplication.findBar.findField.value = params.search;
       }
       // borrowing syntax from "Parameters for Opening PDF Files"
       if (params.has("page")) {
