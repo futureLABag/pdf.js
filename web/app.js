537c350921eab79376d146d1e752f10da0ac42ad
Micha Surber <msurber@futurelab.ch>
2019-05-15 11:23:48 +0200

    Replace export with print and download
    We want to be more flexible with enabling and disabling features.
    We remove the old export option and introduce a print and a download
    option. If set to ‚true‘ the print button will be available or the
    document can be downloaded.
    
    Replace ' to "

--- web/app.js
+++ web/app.js
@@ -2182,13 +2182,17 @@ function reportPageStatsPDFBug({ pageNumber }) {
 function webViewerInitialized() {
   const appConfig = PDFViewerApplication.appConfig;
   let file;
+
   if (typeof PDFJSDev === "undefined" || PDFJSDev.test("GENERIC")) {
     const queryString = document.location.hash.substring(1);
     const params = parseQueryString(queryString);
     file = params.get("file") ?? AppOptions.get("defaultUrl");
 
-    if (params.export === 'true') {
-      document.body.classList.add('fl_export_allowed');
+    if (params.print === "true") {
+      document.body.classList.add("fl_print_allowed");
+    }
+    if (params.download === "true") {
+      document.body.classList.add("fl_download_allowed");
     }
     validateFileURL(file);
   } else if (PDFJSDev.test("MOZCENTRAL")) {
