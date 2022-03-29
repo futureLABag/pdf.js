7b716ef60354ff2d6b67c49f733683848d256376
Oliver Gruber <ogruber@futurelab.ch>
2022-03-29 17:28:49 +0200

    Change default zoom to page-fit

--- web/ui_utils.js
+++ web/ui_utils.js
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 
-const DEFAULT_SCALE_VALUE = "auto";
+const DEFAULT_SCALE_VALUE = "page-fit";
 const DEFAULT_SCALE = 1.0;
 const DEFAULT_SCALE_DELTA = 1.1;
 const MIN_SCALE = 0.1;
